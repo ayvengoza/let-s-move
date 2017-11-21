e826d2eea91a364a9e34c69b3ffb7c34507fb7a6
ayvengoza <ayvengoza@gmail.com>
2017-11-21 10:58:34 +0200

    start application

---
+++ app/src/main/java/com/ayvengoza/activ/letsmove/StartMoveActivity.java
@@ -0,0 +1,13 @@
+package com.ayvengoza.activ.letsmove;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class StartMoveActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_start_move);
+    }
+}

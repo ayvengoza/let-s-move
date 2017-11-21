0465fbb8b7cf0adaeecb4a75e293efd4501be867
ayvengoza <ayvengoza@gmail.com>
2017-11-21 12:01:05 +0200

    add fragment

--- app/src/main/java/com/ayvengoza/activ/letsmove/StartMoveActivity.java
+++ app/src/main/java/com/ayvengoza/activ/letsmove/StartMoveActivity.java
@@ -1,13 +1,13 @@
 package com.ayvengoza.activ.letsmove;
 
+import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class StartMoveActivity extends AppCompatActivity {
+public class StartMoveActivity extends SingleFragmentActivity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_start_move);
+    public Fragment createFragment() {
+        return StartMoveFragment.newInstance();
     }
 }
